da681660e2b03e4d0f212cc0f1543f20194bee75
ichsan <ichsan.alam01@gmail.com>
2025-01-14 10:53:37 +0700

    Coding choose bank feature

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PackageBank;
 use App\Models\PackageTour;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StorePackageBookingRequest;
+use App\Http\Requests\UpdatePackageBookingRequest;
+use App\Models\PackageBooking;
+use Carbon\Carbon;
 
 class FrontController extends Controller
 {
@@ -19,5 +26,68 @@ class FrontController extends Controller
 
     public function book(PackageTour $packageTour) {
         return view('front.book', compact('packageTour'));
+    }
+
+    public function book_store(StorePackageBookingRequest $request, PackageTour $packageTour) {
+        $user = Auth::user();
+        $bank = PackageBank::orderByDesc('id')->first();
+        $packageBookingId = null;
+
+        DB::transaction(function () use ($request, $user, $packageTour, $bank, &$packageBookingId) {
+            $validated = $request->validated();
+
+            $startDate = new Carbon($validated['start_date']);
+            $totalDays = $packageTour->days - 1;
+
+            $endDate = $startDate->addDays($totalDays);
+            $sub_total = $packageTour->price * $validated['quantity'];
+            $insurance = 300000 * $validated['quantity'];
+            $tax = $sub_total * 0.10;
+
+            $validated['end_date'] = $endDate;
+            $validated['user_id'] = $user->id;
+            $validated['is_paid'] = false;
+            $validated['proof'] = 'dummytrx.png';
+            $validated['package_tour_id'] = $packageTour->id;
+            $validated['package_bank_id'] = $bank->id;
+            $validated['insurance'] = $insurance;
+            $validated['tax'] = $tax;
+            $validated['sub_total'] = $sub_total;
+            $validated['total_amount'] = $sub_total + $tax + $insurance;
+
+            $packageBooking = PackageBooking::create($validated);
+            $packageBookingId = $packageBooking->id;
+        });
+
+        if ($packageBookingId) {
+            return redirect()->route('front.choose_bank', $packageBookingId);
+        } else {
+            return back()->withErrors('failed to create booking');
+        }
+    }
+
+    public function choose_bank(PackageBooking $packageBooking) {
+        $user = Auth::user();
+        if($packageBooking->user_id != $user->id) {
+            abort(403);
+        }
+
+        $banks = PackageBank::all();
+        return view('front.choose_bank', compact('packageBooking', 'banks'));
+    }
+
+    public function choose_bank_store(UpdatePackageBookingRequest $request, PackageBooking $packageBooking) {
+        $user = Auth::user();
+        if($packageBooking->user_id != $user->id) {
+            abort(403);
+        }
+
+        DB::transaction(function () use ($request, $packageBooking) {
+            $validated = $request->validated();
+            $packageBooking->update([
+                'package_bank_id' => $validated['package_bank+id'],
+            ]);
+        });
+        return redirect()->route('front.book_payment', $packageBooking->id);
     }
 }

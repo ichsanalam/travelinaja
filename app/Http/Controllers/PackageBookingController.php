fc5922f4a1dc937cd0ec23bc2fb263478bf05ce9
ichsan <ichsan.alam01@gmail.com>
2025-01-15 11:16:05 +0700

    Coding approve transaction feature

--- app/Http/Controllers/PackageBookingController.php
+++ app/Http/Controllers/PackageBookingController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\PackageBooking;
 use Illuminate\Http\Request;
+use App\Models\PackageBooking;
+use Illuminate\Support\Facades\DB;
 
 class PackageBookingController extends Controller
 {
@@ -12,7 +13,8 @@ class PackageBookingController extends Controller
      */
     public function index()
     {
-        //
+        $package_bookings = PackageBooking::with('customer', 'tour')->orderByDesc('id')->paginate(10);
+        return view('admin.package_bookings.index', compact('package_bookings'));
     }
 
     /**
@@ -36,7 +38,7 @@ class PackageBookingController extends Controller
      */
     public function show(PackageBooking $packageBooking)
     {
-        //
+        return view('admin.package_bookings.show', compact('packageBooking'));
     }
 
     /**
@@ -52,7 +54,12 @@ class PackageBookingController extends Controller
      */
     public function update(Request $request, PackageBooking $packageBooking)
     {
-        //
+        DB::transaction(function () use ($packageBooking) {
+            $packageBooking->update([
+                'is_paid' => true,
+            ]);
+        });
+        return redirect()->route('admin.package_bookings.show', $packageBooking);
     }
 
     /**
